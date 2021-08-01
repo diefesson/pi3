df106d19f6f61677b0f726baba465d13bb5e8ff5
Diefesson <diefesson.so@gmail.com>
2021-08-01 11:24:11 -0300

    refactor(frontend): Migrated feed and posts

---
+++ frontend/src/components/feed.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="feed">
+    <post v-for="p in posts" v-bind:key="p.id" v-bind:post="p" />
+  </div>
+</template>
+
+<script>
+import Post from "./post.vue";
+
+export default {
+  name: "Feed",
+  components: { Post },
+  data() {
+    return {
+      posts: [
+        {
+          ong: {
+            name: "Test ONG",
+            image: "https://via.placeholder.com/150",
+          },
+          title: "Post title",
+          image: "https://via.placeholder.com/150",
+        },
+        {
+          ong: {
+            name: "Test ONG",
+            image: "https://via.placeholder.com/150",
+          },
+          title: "Post title",
+          image: "https://via.placeholder.com/150",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style src="../styles/feed.scss" lang="scss" scoped></style>
---
+++ frontend/src/components/post.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="post">
+    <div class="post-top">
+        <img class="post-top-image" v-bind:src="post.ong.image" />
+        <span>{{ post.ong.name }}</span>
+    </div>
+    <img class="post-middle" v-bind:src="post.image" />
+    <div class="post-bottom">SHARE</div>
+  </div>
+</template>
+
+<style src="../styles/post.scss" lang="scss" scoped></style>
+
+<script>
+export default {
+  name: "Post",
+  props: ["post"],
+};
+</script>
