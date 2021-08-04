7d68821c438be425a8733b51572289e4999ac752
loiola0 <victorloiola@alu.ufc.br>
2021-08-04 00:19:22 -0300

    feature(backend): implemented post

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,9 +14,11 @@ app.use(express.static("public"));
 const index = require("./routes/index.js");
 const pet = require("./routes/pet-route");
 const upload = require("./routes/upload-route");
+const post = require("./routes/post-route");
 
 app.use("/", index);
 app.use("/pet", pet);
+app.use("/post", post);
 app.use("/upload", upload);
 app.use((req, res, next) => {
   res.status(404).send({
