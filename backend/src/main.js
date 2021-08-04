83a6059efb5a6c2b455b79d49ef7b35d0cee7c60
Victor Gabriel <61810592+loiola0@users.noreply.github.com>
2021-08-04 17:56:49 -0300

    Update main.js

--- backend/src/main.js
+++ backend/src/main.js
@@ -3,11 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const { urlencoded } = require("body-parser");
 const PostRouter = require("./routers/post-router");
-<<<<<<< HEAD
 const PetRouter = require("./routers/pet-router");
-=======
 const UserRouter = require("./routers/user-router");
->>>>>>> login
+
 
 dotenv.config()
 
@@ -15,12 +13,13 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 
-<<<<<<< HEAD
+
 app.use(cors())
 app.use(urlencoded({ extended: true }));
 app.use("/posts", PostRouter);
 app.use("/pets", PetRouter);
-=======
+app.use("/users", UserRouter);
+
 app.use(express.json());
 app.use(express.urlencoded( {extended: true} ));
 app.use(cors())
@@ -34,7 +33,7 @@ app.use((req, res) => {
         error: "Not found"
     })
 })
->>>>>>> login
+
 
 app.listen(port, () => {
     console.log("server started at port " + String(port));
