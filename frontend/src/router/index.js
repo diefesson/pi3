c868e941a20248c2bf9a3b54b0d86ded129402e8
Diefesson <diefesson.so@gmail.com>
2021-09-02 16:06:25 -0300

    feat(frontend): added redirect for pages that requires sign in

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,7 @@ import ViewPet from '../views/view-pet.vue'
 import EditarPet from '../views/editar-pet.vue'
 import Posts from "../views/posts.vue"
 import ViewPost from "../views/view-post.vue"
+import userService from "../services/user-service"
 
 
 const routes = [
@@ -21,49 +22,82 @@ const routes = [
   {
     path: "/pet/add",
     name: "CadastroPet",
-    component: CadastroPet
+    component: CadastroPet,
+    meta: {
+      requireSignIn: true
+    }
   },
   {
     path: "/employee/add",
     name: "CadastroFuncionario",
-    component: CadastroFuncionario
+    component: CadastroFuncionario,
+    meta: {
+      requireSignIn: true
+    }
   },
   {
     path: "/posts",
     name: "Posts",
-    component: Posts
+    component: Posts,
+    meta: {
+      requireSignIn: true
+    }
   },
   {
     path: "/posts/new",
     name: "AddPost",
-    component: ViewPost
+    component: ViewPost,
+    meta: {
+      requireSignIn: true
+    }
   },
   {
     path: "/posts/:id(\\d+)",
     name: "UpdatePost",
     component: ViewPost,
-    props: true
+    props: true,
+    meta: {
+      requireSignIn: true
+    }
   },
   {
     path: "/pet/list",
     name: "ListarPets",
-    component: ListarPets
+    component: ListarPets,
+    meta: {
+      requireSignIn: true
+    }
   },
   {
     path: "/pet/view",
     name: "ViewPet",
-    component: ViewPet
+    component: ViewPet,
+    meta: {
+      requireSignIn: true
+    }
   },
   {
     path: "/pet/edit",
     name: "EditarPet",
-    component: EditarPet
+    component: EditarPet,
+    meta: {
+      requireSignIn: true
+    }
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((record) => record.meta.requireSignIn)) {
+    if (userService.getSession() == null) {
+      next({ name: "Home" })
+    }
+  }
+  next()
 })
 
 export default router
